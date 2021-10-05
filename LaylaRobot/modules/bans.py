0bcd492db3cbc80eda4f7a11095f07b95ccbbef4
Aripiyok <86340085+Aripiyok@users.noreply.github.com>
2021-10-06 03:10:40 +0700

    Update bans.py

--- LaylaRobot/modules/bans.py
+++ LaylaRobot/modules/bans.py
@@ -110,7 +110,7 @@ def ban(update: Update, context: CallbackContext) -> str:
 
         # bot.send_sticker(chat.id, BAN_STICKER)  # banhammer marie sticker
         reply = (
-            f"<code>❕</code><b>Ban Event</b>\n"
+            f"<code>🔥</code><b>MAMPUS KEBAN!!!xixixi</b>\n"
             f"<code> </code><b>•  User:</b> {mention_html(member.user.id, html.escape(member.user.first_name))}"
         )
         if reason:
