2692933c78a36a872618d4c308fa1af39bbe8a79
Aripiyok <86340085+Aripiyok@users.noreply.github.com>
2021-10-06 02:41:38 +0700

    Update ping.py

--- LaylaRobot/modules/ping.py
+++ LaylaRobot/modules/ping.py
@@ -80,9 +80,9 @@ def ping(update: Update, context: CallbackContext):
     uptime = get_readable_time((time.time() - StartTime))
 
     message.edit_text(
-        "PONG!!\n"
-        "<b>Time Taken:</b> <code>{}</code>\n"
-        "<b>Service uptime:</b> <code>{}</code>".format(telegram_ping, uptime),
+        "BOY!!\n"
+        "<b>BOY!! Time taken:</b> <code>{}</code>\n"
+        "<b>BOY!! uptime:</b> <code>{}</code>".format(telegram_ping, uptime),
         parse_mode=ParseMode.HTML,
     )
 
